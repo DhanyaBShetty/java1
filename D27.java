44766d1afb7673da87896848393467c3b8f0f28c
DhanyaBShetty <169141968+DhanyaBShetty@users.noreply.github.com>
2024-11-12 15:31:36 +0530

    class Tools created method reverse()

---
+++ D27.java
@@ -0,0 +1,12 @@
+
+package pkg1;
+
+import pkg2.Tools2;
+public class D27 {
+    public static void main(String[] args){
+        Tools2 t1=new Tools2();
+        System.out.println(t1.reverse("Dhanya"));
+        
+    }
+    
+}
