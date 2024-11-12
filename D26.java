59aab535b4856bb02292f128dd84d8b1541892d9
DhanyaBShetty <169141968+DhanyaBShetty@users.noreply.github.com>
2024-11-12 15:30:35 +0530

    method reverse() to reverse a string

---
+++ D26.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+
+import pkg1.Tools;
+
+
+
+public class D26 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        System.out.println(t1.reverse("Dhanya"));
+    }
+    
+}
